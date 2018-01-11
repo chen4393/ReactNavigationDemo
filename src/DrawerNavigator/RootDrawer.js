99202be18691440775f62883e1b1c057d65241c2
Macbook <Macbook@Gazelles-MacBook-3.local>
2018-01-11 17:59:28 -0600

    Added icons on drawer

--- src/DrawerNavigator/RootDrawer.js
+++ src/DrawerNavigator/RootDrawer.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { HomeScreen, ProfileScreen } from './Screens';
 
 const RootDrawer = DrawerNavigator({
   Home: {
     screen: HomeScreen,
+    navigationOptions: {
+      drawerLabel: 'Home',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-home' : 'ios-home-outline'}
+          size={20}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
   },
   Profile: {
     screen: ProfileScreen,
+    navigationOptions: {
+      drawerLabel: 'Profile',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-person' : 'ios-person-outline'}
+          size={20}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
   },
 });
 
