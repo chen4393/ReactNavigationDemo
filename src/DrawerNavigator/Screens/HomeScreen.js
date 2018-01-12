96a21b1612089554a6df1ff16d8aa7ceb8cc45d4
Macbook <Macbook@Gazelles-MacBook-3.local>
2018-01-12 12:32:35 -0600

    Added toggle button

--- src/DrawerNavigator/Screens/HomeScreen.js
+++ src/DrawerNavigator/Screens/HomeScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
-const HomeScreen = () => (
+const HomeScreen = ({ navigation }) => (
   <View style={styles.container}>
     <Text style={styles.textStyle}>Home Screen</Text>
+    <Button
+      onPress={() => navigation.navigate('DrawerToggle')}
+      title="Open Drawer"
+    />
   </View>
 );
 
