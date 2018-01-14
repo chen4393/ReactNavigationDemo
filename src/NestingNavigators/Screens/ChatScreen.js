14643593454f6cb2089059f59848da5273628b43
ccr13 <chen4393@umn.edu>
2018-01-14 01:21:42 -0600

    Defined navigation options

--- src/NestingNavigators/Screens/ChatScreen.js
+++ src/NestingNavigators/Screens/ChatScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class ChatScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: `Chat with ${navigation.state.params.user}`,
-  });
+  // static navigationOptions = ({ navigation }) => ({
+  //   title: `Chat with ${navigation.state.params.user}`,
+  //   headerRight: <Button title="info" />
+  // });
+  static navigationOptions = ({ navigation }) => {
+    const { state, setParams } = navigation;
+    const isInfo = state.params.mode === 'info';
+    const { user } = state.params;
+    return {
+      title: isInfo ? `${user}'s Contact Info` : `Chat with ${state.params.user}`,
+      headerRight: (
+        <Button
+          title={isInfo ? 'Done' : `${user}'s info`}
+          onPress={() => setParams({ mode: isInfo ? 'none' : 'info' })}
+        />
+      ),
+    };
+  };
   render() {
     const { params } = this.props.navigation.state;
     return (
